2a3b3489da9da29b55a176773644f42d7f4e224b
statiolake <statiolake@gmail.com>
2022-07-01 16:10:04 +0900

    fix: fix Code Action

--- src/omnisharp/fileOperationsResponseEditBuilder.ts
+++ src/omnisharp/fileOperationsResponseEditBuilder.ts
@@ -4,29 +4,19 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'coc.nvim';
-import { Uri } from 'coc.nvim';
 import { LanguageMiddlewareFeature } from './LanguageMiddlewareFeature';
 import { FileModificationType, FileOperationResponse, ModifiedFileResponse, RenamedFileResponse } from "./protocol";
 import { toRange2 } from './typeConversion';
 
 export async function buildEditForResponse(changes: FileOperationResponse[], languageMiddlewareFeature: LanguageMiddlewareFeature, token: vscode.CancellationToken): Promise<boolean> {
-    let edit: vscode.WorkspaceEdit = { changes: {}, documentChanges: [] };
-
-    let fileToOpen: Uri | undefined;
+    let editChanges: vscode.WorkspaceEdit = { changes: {} };
+    let editDocumentChanges: vscode.WorkspaceEdit = { documentChanges: [] };
 
     if (!changes || !Array.isArray(changes) || !changes.length) {
         return true;
     }
 
     for (const change of changes) {
-        if (change.ModificationType == FileModificationType.Opened) {
-            // The CodeAction requested that we open a file.
-            // Record that file name and keep processing CodeActions.
-            // If a CodeAction requests that we open multiple files
-            // we only open the last one (what would it mean to open multiple files?)
-            fileToOpen = vscode.Uri.file(change.FileName);
-        }
-
         if (change.ModificationType == FileModificationType.Modified) {
             const modifiedChange = <ModifiedFileResponse>change;
             const uri = vscode.Uri.file(modifiedChange.FileName);
@@ -35,14 +25,14 @@ export async function buildEditForResponse(changes: FileOperationResponse[], lan
                 edits.push(vscode.TextEdit.replace(toRange2(textChange), textChange.NewText));
             }
 
-            edit.changes[uri.toString()] = edits;
+            editChanges.changes[uri.toString()] = edits;
         }
     }
 
     for (const change of changes) {
         if (change.ModificationType == FileModificationType.Renamed) {
             const renamedChange = <RenamedFileResponse>change;
-            edit.documentChanges.push({
+            editDocumentChanges.documentChanges.push({
                 kind: 'rename',
                 oldUri: vscode.Uri.file(renamedChange.FileName).toString(),
                 newUri: vscode.Uri.file(renamedChange.NewFileName).toString(),
@@ -51,18 +41,15 @@ export async function buildEditForResponse(changes: FileOperationResponse[], lan
     }
 
     // Allow language middlewares to re-map its edits if necessary.
-    edit = await languageMiddlewareFeature.remap("remapWorkspaceEdit", edit, token);
+    editChanges = await languageMiddlewareFeature.remap(
+        "remapWorkspaceEdit", editChanges, token
+    );
+    editDocumentChanges = await languageMiddlewareFeature.remap(
+        "remapWorkspaceEdit", editDocumentChanges, token
+    );
 
-    const applyEditPromise = vscode.workspace.applyEdit(edit);
-
-    // Unfortunately, the textEditor.Close() API has been deprecated
-    // and replaced with a command that can only close the active editor.
-    // If files were renamed that weren't the active editor, their tabs will
-    // be left open and marked as "deleted" by VS Code
-    return fileToOpen !== undefined
-        ? applyEditPromise.then(res => {
-            return vscode.commands.executeCommand("vscode.open", fileToOpen)
-                .then(_ => res);
-        })
-        : applyEditPromise;
+    let res = true;
+    res = await vscode.workspace.applyEdit(editDocumentChanges) && res;
+    res = await vscode.workspace.applyEdit(editChanges) && res;
+    return res;
 }
